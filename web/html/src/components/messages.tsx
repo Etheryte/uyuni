578585dd2e81f73940b290aa2a448dc48ccb2db9
Etheryte <karl.tarvas@suse.com>
2021-01-11 14:24:29 +0200

    Try migrate

--- web/html/src/components/messages.tsx
+++ web/html/src/components/messages.tsx
@@ -1,26 +1,23 @@
-// @flow
 /* eslint-disable */
 import * as React from "react";
 
 type Severity = "info" | "success" | "warning" | "error";
 
 export type ServerMessageType = {
-  severity: Severity,
-  text: string,
-  args: Array<string>
-}
+    severity: Severity;
+    text: string;
+    args: Array<string>;
+};
 
 export type MessageType = {
-  severity: Severity,
-  text: string |
-    React.Node |
-    Array<React.Node | string> // TODO only for compatibility, deprecate it
-}
+    severity: Severity;
+    text: string | React.ReactNode | Array<React.ReactNode | string>; // TODO only for compatibility, deprecate it
+};
 
 type Props = {
-  /** Message objects to display */
-  items: Array<MessageType> | MessageType
-}
+    /** Message objects to display */
+    items: Array<MessageType> | MessageType;
+};
 
 /**
  * Component to render multiple alert messages.
@@ -53,78 +50,81 @@ type Props = {
  * ```
  */
 const _classNames = {
-    "error": "danger",
-    "success": "success",
-    "info": "info",
-    "warning": "warning",
-}
+    error: "danger",
+    success: "success",
+    info: "info",
+    warning: "warning",
+};
 
 export class Messages extends React.Component<Props> {
-
-
-    static info(text: string | React.Node): MessageType {
-      return Messages.message("info", text);
+    static info(text: string | React.ReactNode): MessageType {
+        return Messages.message("info", text);
     }
 
-    static success(text: string | React.Node): MessageType {
-      return Messages.message("success", text);
+    static success(text: string | React.ReactNode): MessageType {
+        return Messages.message("success", text);
     }
 
-    static error(text: string | React.Node): MessageType {
-      return Messages.message("error", text);
+    static error(text: string | React.ReactNode): MessageType {
+        return Messages.message("error", text);
     }
 
-    static warning(text: string | React.Node): MessageType {
-      return Messages.message("warning", text);
+    static warning(text: string | React.ReactNode): MessageType {
+        return Messages.message("warning", text);
     }
 
-    static message(severityIn: Severity, textIn: string | React.Node): MessageType {
+    static message(severityIn: Severity, textIn: string | React.ReactNode): MessageType {
         return {severity: severityIn, text: textIn};
     }
 
     render() {
         const items: Array<MessageType> = Array.isArray(this.props.items) ? this.props.items : [this.props.items];
 
-        var msgs = items.map((item, index) =>
-          <div key={"msg" + index} className={'alert alert-' + _classNames[item.severity]}>
-            { Array.isArray(item.text) ? 
-              item.text.map(txt => <div>{txt}</div>): item.text }
-          </div>
-        );
+        var msgs = items.map((item, index) => (
+            <div key={"msg" + index} className={"alert alert-" + _classNames[item.severity]}>
+                {Array.isArray(item.text) ? item.text.map(txt => <div>{txt}</div>) : item.text}
+            </div>
+        ));
 
-        return (<div key={"messages-pop-up"}>{msgs}</div>);
+        return <div key={"messages-pop-up"}>{msgs}</div>;
     }
-
 }
 
-export const fromServerMessage = (message: ServerMessageType, messageMap?: {[string]: string | ((any) => string | React.Node)}): ?MessageType => {
-  let messageText = message.text;
-  if (messageMap && (messageText in messageMap)) {
-    messageText = messageMap[message.text];
-    if (typeof messageText === "function") {
-      messageText = messageText(message.args);
+export const fromServerMessage = (
+    message: ServerMessageType,
+    messageMap?: {
+        [key: string]: string | ((arg0: any) => string | React.ReactNode);
     }
-  }
-  let msg: ?MessageType;
-  switch (message.severity) {
-    case "info":
-      msg = Messages.info(messageText);
-      break;
-    case "success":
-      msg = Messages.success(messageText);
-      break;
-    case "warning":
-      msg = Messages.warning(messageText);
-      break;
-    case "error":
-      msg = Messages.error(messageText);
-      break;
-  }
-  return msg;
-}
+): MessageType | null | undefined => {
+    let messageText = message.text;
+    if (messageMap && messageText in messageMap) {
+        messageText = messageMap[message.text];
+        if (typeof messageText === "function") {
+            messageText = messageText(message.args);
+        }
+    }
+    let msg: MessageType | null | undefined;
+    switch (message.severity) {
+        case "info":
+            msg = Messages.info(messageText);
+            break;
+        case "success":
+            msg = Messages.success(messageText);
+            break;
+        case "warning":
+            msg = Messages.warning(messageText);
+            break;
+        case "error":
+            msg = Messages.error(messageText);
+            break;
+    }
+    return msg;
+};
 
-function msg(severityIn: Severity, ...textIn: Array<React.Node>) {
-    return textIn.map(function(txt) {return {severity: severityIn, text: textIn}});
+function msg(severityIn: Severity, ...textIn: Array<React.ReactNode>) {
+    return textIn.map(function(txt) {
+        return {severity: severityIn, text: textIn};
+    });
 }
 
 /**
@@ -134,16 +134,16 @@ function msg(severityIn: Severity, ...textIn: Array<React.Node>) {
  * of the `Messages` component.
  */
 export const Utils = {
-  info: function (textIn: string | React.Node | Array<string | React.Node>): Array<MessageType> {
-    return msg("info", textIn);
-  },
-  success: function (textIn: string | React.Node | Array<string | React.Node>): Array<MessageType> {
-    return msg("success", textIn);
-  },
-  warning: function (textIn: string | React.Node | Array<string | React.Node>): Array<MessageType> {
-    return msg("warning", textIn);
-  },
-  error: function (textIn: string | React.Node | Array<string | React.Node>): Array<MessageType> {
-    return msg("error", textIn);
-  }
-}
+    info: function(textIn: string | React.ReactNode | Array<string | React.ReactNode>): Array<MessageType> {
+        return msg("info", textIn);
+    },
+    success: function(textIn: string | React.ReactNode | Array<string | React.ReactNode>): Array<MessageType> {
+        return msg("success", textIn);
+    },
+    warning: function(textIn: string | React.ReactNode | Array<string | React.ReactNode>): Array<MessageType> {
+        return msg("warning", textIn);
+    },
+    error: function(textIn: string | React.ReactNode | Array<string | React.ReactNode>): Array<MessageType> {
+        return msg("error", textIn);
+    },
+};
